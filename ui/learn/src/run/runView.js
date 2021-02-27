75b15e8ee0c6db953b621b73eabebfc063af47e6
justremember <nothingpersonal.qwerty@gmail.com>
2021-02-27 20:11:42 +0800

    add pocket to learn puzzles (html + css only). minor fixes

--- ui/learn/src/run/runView.js
+++ ui/learn/src/run/runView.js
@@ -8,6 +8,7 @@ var stageComplete = require("./stageComplete");
 var renderPromotion = require("../promotion").view;
 var renderProgress = require("../progress").view;
 var makeStars = require("../progress").makeStars;
+var renderPocket = require("../crazy/crazyView").renderPocket;
 
 function renderFailed(ctrl) {
   return m(
@@ -84,6 +85,7 @@ module.exports = function (ctrl) {
         renderPromotion(ctrl, level),
         renderInfo(ctrl),
       ]),
+      renderPocket(ctrl, "top", level.blueprint.hasPocket),
       m("div.learn__table", [
         m("div.wrap", [
           m("div.title", [
@@ -106,6 +108,7 @@ module.exports = function (ctrl) {
           renderProgress(ctrl.progress),
         ]),
       ]),
+      renderPocket(ctrl, "bottom", level.blueprint.hasPocket),
     ]
   );
 };
