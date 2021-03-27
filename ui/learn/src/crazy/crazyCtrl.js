916082cd27981e0c57e1703bc25eef4a5471b54e
justremember <nothingpersonal.qwerty@gmail.com>
2021-03-27 21:39:20 +0800

    make pocket piece in learn draggable (breaks after placing piece)

--- ui/learn/src/crazy/crazyCtrl.js
+++ ui/learn/src/crazy/crazyCtrl.js
@@ -1,5 +1,19 @@
+var ground = require("../ground");
+var ogDrag = require("../og/drag");
+
 exports.drag = function(ctrl, color, e) {
   console.log("drag");
+  console.log("ground.instance.data", ground.instance.data);
+  if (e.button !== undefined && e.button !== 0) return; // only touch or left click
+  if (ground.instance.data.movable.color !== color) return;
+  var el = e.target,
+    role = el.getAttribute("data-role"),
+    number = el.getAttribute("data-nb");
+  console.log(el, role, number);
+  if (!role || !color || number === "0") return;
+  e.stopPropagation();
+  e.preventDefault();
+  ogDrag.dragNewPiece(ground.instance.data, { color: color, role: role }, e);
 };
 
 exports.selectToDrop = function(ctrl, color, e) {
