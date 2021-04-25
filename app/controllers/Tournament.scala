lichess-org/lila

new api endpoint to update team battles

app/controllers/Tournament.scala
@@ -316,7 +316,7 @@ final class Tournament(
     }
 
   def apiCreate =
-    ScopedBody() { implicit req => me =>
+    ScopedBody(_.Tournament.Write) { implicit req => me =>
       if (me.isBot || me.lame) notFoundJson("This account cannot create tournaments")
       else doApiCreate(me)
     }
@@ -388,6 +388,37 @@ final class Tournament(
                     Redirect(routes.Tournament.show(tour.id))
               )
           case tour => Redirect(routes.Tournament.show(tour.id)).fuccess
+        }
+      }
+    }
+
+  def apiTeamBattleUpdate(id: String) =
+    ScopedBody(_.Tournament.Write) { implicit req => me =>
+      implicit def lang = reqLang
+      repo byId id flatMap {
+        _ ?? {
+          case tour if (tour.createdBy == me.id || isGranted(_.ManageTournament, me)) && !tour.isFinished =>
+            lila.tournament.TeamBattle.DataForm.empty
+              .bindFromRequest()
+              .fold(
+                newJsonFormError,
+                res =>
+                  api.teamBattleUpdate(tour, res, env.team.api.filterExistingIds) >> {
+                    repo byId tour.id map (_ | tour) flatMap { tour =>
+                      jsonView(
+                        tour,
+                        none,
+                        none,
+                        getUserTeamIds = getUserTeamIds,
+                        env.team.getTeamName,
+                        none,
+                        none,
+                        partial = false
+                      )
+                    } map { Ok(_) }
+                  }
+              )
+          case _ => BadRequest(jsonError("Can't update that tournament.")).fuccess
         }
       }
     }

modules/tournament/src/main/TeamBattle.scala
@@ -77,8 +77,14 @@ object TeamBattle {
         teams: String,
         nbLeaders: Int
     ) {
-      def potentialTeamIds: Set[TeamID] =
-        teams.linesIterator.map(_.takeWhile(' ' !=)).filter(_.nonEmpty).toSet
+      // guess if newline or comma separated
+      def potentialTeamIds: Set[TeamID] = {
+        val lines = teams.linesIterator.toList
+        val dirtyIds =
+          if (lines.sizeIs > 1) lines.map(_.takeWhile(' ' !=))
+          else lines.headOption.??(_.split(',').toList)
+        dirtyIds.map(_.trim).filter(_.nonEmpty).toSet
+      }
     }
   }
 }

TheYoBots/lishogi

app/controllers/Tournament.scala
@@ -289,7 +289,7 @@ object Tournament extends LidraughtsController {
     }
   }
 
-  def apiCreate = ScopedBody() { implicit req => me =>
+  def apiCreate = ScopedBody(_.Tournament.Write) { implicit req => me =>
     if (me.isBot || me.lame) notFoundJson("This account cannot create tournaments")
     else teamsIBelongTo(me) flatMap { teams => doApiCreate(me, teams) }
   }
@@ -299,7 +299,8 @@ object Tournament extends LidraughtsController {
       jsonFormErrorDefaultLang,
       setup => rateLimitCreation(me, setup.password.isDefined, req) {
         env.api.createTournament(setup, me, teams, getUserTeamIds, andJoin = false) flatMap { tour =>
-          Env.tournament.jsonView(tour, none, none, getUserTeamIds, Env.team.cached.name, none, none, partial = false, lidraughts.i18n.defaultLang, none)
+          val lang = me.lang flatMap lidraughts.i18n.I18nLangPicker.byStr getOrElse lidraughts.i18n.defaultLang
+          env.jsonView(tour, none, none, getUserTeamIds, Env.team.cached.name, none, none, partial = false, lang, none)
         } map { Ok(_) }
       }
     )
@@ -338,6 +339,28 @@ object Tournament extends LidraughtsController {
     }
   }
 
+  def apiTeamBattleUpdate(id: String) =
+    ScopedBody(_.Tournament.Write) { implicit req => me =>
+      repo byId id flatMap {
+        _ ?? {
+          case tour if (tour.createdBy == me.id || isGranted(_.ManageTournament, me)) && !tour.isFinished =>
+            lidraughts.tournament.TeamBattle.DataForm.empty
+              .bindFromRequest()
+              .fold(
+                jsonFormErrorDefaultLang,
+                res =>
+                  env.api.teamBattleUpdate(tour, res, Env.team.api.filterExistingIds) >> {
+                    repo byId tour.id map (_ | tour) flatMap { tour =>
+                      val lang = me.lang flatMap lidraughts.i18n.I18nLangPicker.byStr getOrElse lidraughts.i18n.defaultLang
+                      env.jsonView(tour, none, none, getUserTeamIds, Env.team.cached.name, none, none, partial = false, lang, none)
+                    } map { Ok(_) }
+                  }
+              )
+          case _ => BadRequest(jsonError("Can't update that tournament.")).fuccess
+        }
+      }
+    }
+
   def limitedInvitation = Auth { implicit ctx => me =>
     for {
       (tours, _) <- upcomingCache.get
@@ -357,7 +380,7 @@ object Tournament extends LidraughtsController {
     negotiate(
       html = notFound,
       api = _ =>
-        Env.tournament.cached.promotable.get.nevermind map {
+        env.cached.promotable.get.nevermind map {
           lidraughts.tournament.Spotlight.select(_, ctx.me, 4)
         } flatMap env.apiJsonView.featured map { Ok(_) }
     )
@@ -418,7 +441,7 @@ object Tournament extends LidraughtsController {
       repo byId id flatMap {
         _ ?? { tour =>
           tour.isTeamBattle ?? {
-            Env.tournament.cached.battle.teamStanding.get(tour.id) map { standing =>
+            env.cached.battle.teamStanding.get(tour.id) map { standing =>
               Ok(views.html.tournament.teamBattle.standing(tour, standing))
             }
           }

modules/tournament/src/main/TeamBattle.scala
@@ -70,8 +70,14 @@ object TeamBattle {
         teams: String,
         nbLeaders: Int
     ) {
-      def potentialTeamIds: Set[String] =
-        teams.lines.map(_.takeWhile(' ' !=)).filter(_.nonEmpty).toSet
+      // guess if newline or comma separated
+      def potentialTeamIds: Set[String] = {
+        val lines = teams.lines.toList
+        val dirtyIds =
+          if (lines.size > 1) lines.map(_.takeWhile(' ' !=))
+          else lines.headOption.??(_.split(',').toList)
+        dirtyIds.map(_.trim).filter(_.nonEmpty).toSet
+      }
     }
   }
 }